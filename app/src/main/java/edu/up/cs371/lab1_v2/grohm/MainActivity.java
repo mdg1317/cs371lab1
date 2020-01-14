1c769654954f51a3118567580433a9bcc14f0625
mdg1317 <matthew.dennis137@gmail.com>
2020-01-13 20:45:28 -0800

    Initial commit

---
+++ app/src/main/java/edu/up/cs371/lab1_v2/grohm/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.up.cs371.lab1_v2.grohm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
